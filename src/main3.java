46888d0faa19f14023eb8bd6d643b0423ea03c55
olik <olegkhomaa@gmail.com>
2023-03-01 00:27:30 +0200

    Initial commit

---
+++ src/main3.java
@@ -0,0 +1,11 @@
+public class main3 {
+    public static void main(String[] args) {
+
+
+        double number = 3.1434;
+
+
+        int r = (int) Math.round(number);
+        System.out.println("Сума цифр числа " + r);
+    }
+}
