160b209fc94a995d1d123c651526486dfe4de047
olik8246 <126155704+olik8246@users.noreply.github.com>
2023-03-08 19:25:44 +0200

    Update main2.java

--- src/main2.java
+++ src/main2.java
@@ -1,9 +1,9 @@
 public class main2 {
     public static void main(String[] args) {
         int n = 21;
-        double f = n / 10; // перша цифра
+        int f = n / 10; // перша цифра
         int s = n % 10; // друга цифра
-        double sum = f + s;
+        int sum = f + s;
         System.out.println("Сума цифр числа " + n + ": " + sum);
     }
 }
